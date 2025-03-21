768aa80a5214a77a824a674754174f7ba2093054
Jonathan Ung <jonathan.keith.ung@gmail.com>
2025-03-20 19:57:31 -0700

    chore(multiscript) scripting for tests

--- test_all_models.sh
+++ test_all_models.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 # Test all models
-python3 ./test_models.py --model_version v8 --output_dir results --save_txt --save_imgs
-python3 ./test_models.py --model_version v5 --output_dir results --save_txt --save_imgs
-python3 ./test_models.py --model_version v3 --output_dir results --save_txt --save_imgs      
+# python3 ./test_models.py --model_version v8 --output_dir results --save_txt --save_imgs
+# python3 ./test_models.py --model_version v5 --output_dir results --save_txt --save_imgs
+# python3 ./test_models.py --model_version v3 --output_dir results --save_txt --save_imgs      
+
+python3 ./scripts/yolo8_test.py
+python3 ./scripts/yolo5_test.py
+python3 ./scripts/yolo3_test.py
