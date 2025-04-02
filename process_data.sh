390d3dfba8179272a3355c5fc1c53fafc4204574
Nicholas Tam <nta59@sfu.ca>
2025-04-02 16:01:33 -0700

    fix(process_data.sh) corrected to python3 keyword. Corrected path of light_counter call.

--- process_data.sh
+++ process_data.sh
@@ -3,24 +3,24 @@
 # These script takes the resulting data and processes it into relevant CSV files for the data analysis
 
 # Convert the YOLO txt files to CSV files
-python ./data_processing_scripts/lisa_yolo_val_label2csv.py
+python3 ./data_processing_scripts/lisa_yolo_val_label2csv.py
 
 # Convert the Processed YOLOxLISA data to a CSV file
-python ./data_processing_scripts/lisa_processed_label2csv.py
+python3 ./data_processing_scripts/lisa_processed_label2csv.py
 
 # The two scripts above are to be run first, because the matching script relies on the output of the two above
 
 # Sort the label CSVs for processing
-python ./data_processing_scripts/sort_label_csv.py
+python3 ./data_processing_scripts/sort_label_csvs.py
 
 # Match the YOLO and Processed YOLOxLISA data
-python ./data_processing_scripts/matching.py
+python3 ./data_processing_scripts/matching.py
 
 # Copy the matched CSV files to the data/matched_csv directory
 bash ./data_processing_scripts/copy_proc_lisa.sh
 
 # Count the number of lights in the data
-python ./data_analysis_scripts/light_counter.py
+python3 ./data_processing_scripts/light_counter.py
 
 # Format the Spark output
-python ./data_processing_scripts/format_spark_verification.py
+python3 ./data_processing_scripts/format_spark_verification.py
