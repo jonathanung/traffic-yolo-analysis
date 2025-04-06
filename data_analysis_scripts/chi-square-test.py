fc453c6de1665169063265e131dd3f90c4992d9a
Nicholas Tam <nta59@sfu.ca>
2025-04-06 14:23:35 -0700

    feat(chi-square-test.py) added post-hoc like analysis.

--- data_analysis_scripts/chi-square-test.py
+++ data_analysis_scripts/chi-square-test.py
@@ -20,13 +20,30 @@ def load_data(data_dir:Path, models:list,):
     dfs = pd.concat(dfs)
     return dfs
 
+def three_way_chi_square(data:pd.DataFrame):
+    yolov3_data = data.iloc[[0]]
+    yolov5_data = data.iloc[[1]]
+    yolov8_data = data.iloc[[2]]
+
+    chi2, v3_v5p , _, _ = stats.chi2_contingency(pd.concat([yolov3_data,yolov5_data]))
+    chi2, v3_v8p , _, _ = stats.chi2_contingency(pd.concat([yolov3_data,yolov8_data]))
+    chi2, v5_v8p , _, _ = stats.chi2_contingency(pd.concat([yolov5_data,yolov8_data]))
+
+    return [v3_v5p, v3_v8p, v5_v8p]
+
 def main():
     models = ["3", "5", "8"]
     data_dir = Path("data/matched_csv")
     data = load_data(data_dir, models)
-    print(data.aslist)
-    chi2, p, dof, expected = stats.chi2_contingency(data.values)
-    print(f"Chi-squared: {chi2}, p-value: {p}, degrees of freedom: {dof}, expected: {expected}")
+    _, p, _, _ = stats.chi2_contingency(data)
+
+    pval = three_way_chi_square(data)
+    print(f'''
+    p-value for all: {p}
+    p-value for v3 and v5: {pval[0]}
+    p-value for v3 and v8: {pval[1]}
+    p-value for v5 and v8: {pval[2]}
+''')
 
 
 if __name__ == "__main__":
